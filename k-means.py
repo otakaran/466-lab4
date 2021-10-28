e9ec70b057ca4ad994d94dd3c453a93589dad7b7
Notchanator <55802661+Notchanator@users.noreply.github.com>
2021-10-28 16:27:32 -0700

    error free code

--- k-means.py
+++ k-means.py
@@ -2,40 +2,31 @@ import pandas as pd
 import numpy as np
 import random
 from sklearn.metrics.pairwise import euclidean_distances
-import sklearn.cluster 
 
-"""
-def k_means(data):
-    domains = [None] * len(data.columns)
-    for column in range(len(data.columns)):
-        domains[column] = (data.iloc[:,column].min(), data.iloc[:,column].max())
-    k = 10
-    centroids = [[] for i in range(k):
-    for column in range(len(centroids)):
-        centroids[column] = random.randint(int(domains[column][0]), int(domains[column][1]))
-    print(centroids)
+
+def k_means(data, k):
+    domains = [None] * len(X.columns)
+    for column in range(len(X.columns)):
+        domains[column] = (X.iloc[:,column].min(), X.iloc[:,column].max())
+    centroids = [[] for i in range(k)]
+    for cluster in range(len(centroids)):
+        for column in range(len(domains)):
+            centroids[cluster].append(data.iloc[random.randint(0, len(X)), column])
     epochs = 1
-    while epochs <= 1000:
+    while epochs <= 100:
         clusters = [[] for j in range(k)]
         for row in range(len(data)):
             dist_big = 0
             big_cluster = None
             for cluster in range(len(centroids)):
-                print(centroids[cluster])
-                dist = euclidean_distances(data.iloc[row], centroids[cluster])
+                dist = euclidean_distances(np.array(X.iloc[row]).reshape(1,-1), np.array(centroids[cluster]).reshape(1,-1))
                 if dist > dist_big:
                     dist_big = dist
                     big_cluster = cluster
             clusters[big_cluster].append(row)
-        old_clusters = clusters
-        for cluster in range(len(clusters)):
-            clusters[cluster] = data.iloc[old_clusters].mean()
-        if (clusters == old_clusters).all():
+        old_centroids = centroids
+        for centroid in range(len(centroids)):
+            centroids[centroid] = np.array(X.iloc[clusters[centroid]].mean())
+        if (centroids == old_centroids):
             break
-    print(clusters)
-"""
 
-data = pd.read_csv("filledData.csv")
-y = data[1]
-X = data[-1]
-print(X)
