0eb3ac02274be1f37a0df02f94ad2312c6ee14f8
Notchanator <55802661+Notchanator@users.noreply.github.com>
2021-10-30 18:48:27 -0700

    k-means
    k-means is done besides exit cases

--- k-means.py
+++ k-means.py
@@ -2,31 +2,38 @@ import pandas as pd
 import numpy as np
 import random
 from sklearn.metrics.pairwise import euclidean_distances
+import matplotlib.pyplot as plt
 
+def select_Centroids(data, k):
+    X = data.copy()
+    data_center = X.mean()
+    centroids = [[] for i in range(k)]
+    index = euclidean_distances(X, [data_center]).argmax()
+    centroids[0] = list(X.iloc[index])
+    X = X.drop(index)
+    for cluster in range(1, k):
+        index = euclidean_distances(X, centroids[:cluster]).sum(axis=1).argmax()
+        centroids[cluster] = list(X.iloc[index])
+        X = X.drop(index)
+    return centroids
 
 def k_means(data, k):
-    domains = [None] * len(X.columns)
-    for column in range(len(X.columns)):
-        domains[column] = (X.iloc[:,column].min(), X.iloc[:,column].max())
-    centroids = [[] for i in range(k)]
-    for cluster in range(len(centroids)):
-        for column in range(len(domains)):
-            centroids[cluster].append(data.iloc[random.randint(0, len(X)), column])
+    X = data.copy()
+    centroids = select_Centroids(data, k)
     epochs = 1
-    while epochs <= 100:
-        clusters = [[] for j in range(k)]
-        for row in range(len(data)):
-            dist_big = 0
-            big_cluster = None
-            for cluster in range(len(centroids)):
-                dist = euclidean_distances(np.array(X.iloc[row]).reshape(1,-1), np.array(centroids[cluster]).reshape(1,-1))
-                if dist > dist_big:
-                    dist_big = dist
-                    big_cluster = cluster
-            clusters[big_cluster].append(row)
-        old_centroids = centroids
+    while epochs <= 300:
+        clusters = euclidean_distances(X, centroids).argmin(axis=1)
+        old_centroids = centroids.copy()
         for centroid in range(len(centroids)):
-            centroids[centroid] = np.array(X.iloc[clusters[centroid]].mean())
-        if (centroids == old_centroids):
+            centroids[centroid] = np.array(X.iloc[clusters == centroid].mean())
+        if np.equal(centroids, old_centroids).all():
             break
+    return clusters
+
+data = pd.read_csv("resources/datasets/4clusters.csv")
+
+clusters = pd.concat([data, pd.Series(k_means(data, 3), name="clusters")], axis=1)
+
+plt.scatter(clusters.iloc[:,0], clusters.iloc[:,1], c=clusters.clusters)
+plt.show()
 
